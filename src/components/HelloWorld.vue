f9e306762cb0cec5643b4e9aa0f8e1ca0aca7df3
polymatter <jlake.jobs@gmail.com>
2019-08-29 22:57:12 +0100

    basic prototype for general interface

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,23 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1>Subject</h1>
+    <select v-model="subjects" multiple>
+      <option>I</option>
+      <option>Jeffrey</option>
+      <option>Trish</option>
+      <option>Catherine</option>
+      <option>John</option>
+    </select>
+    <h1>Predicate</h1>
+    <h2>Verb</h2>
+    <select v-model="verb">
+      <option>to sing</option>
+      <option>to eat</option>
+      <option>to wash</option>
+      <option>to dance</option>
+    </select>
+    <hr/>
+    {{ sentence }}
   </div>
 </template>
 
@@ -35,6 +26,49 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data: function() {
+    return {
+      subjects: [],
+      verb: "",
+      verbConjugate: {
+        "to sing": function(firstperson, plural) {
+          let base = 'to sing'.replace('to ', '');
+          let suffix = firstperson || plural ? '' : 's';
+          return base + suffix;
+        },
+        "to eat": function(firstperson, plural) {
+          let base = 'to eat'.replace('to ', '');
+          let suffix = firstperson || plural ? '' : 's';
+          return base + suffix;
+        },
+        "to wash": function(firstperson, plural) {
+          let base = 'to wash'.replace('to ', '');
+          let suffix = firstperson || plural ? '' : 's';
+          return base + suffix;
+        },
+        "to dance": function(firstperson, plural) {
+          let base = 'to dance'.replace('to ', '');
+          let suffix = firstperson || plural ? '' : 's';
+          return base + suffix;
+        }
+      }
+    }
+  },
+  computed: {
+    sentence: function() {
+      if (this.subjects.length > 0 && this.verb.length > 0) {
+        let firstperson = this.subjects[0] === "I" && this.subjects.length === 1;
+        let plural = this.subjects.length > 1
+        let _subjects = this.subjects;
+        if (plural) {
+          _subjects = this.subjects.map(s => s.replace(/^I$/, 'Me'));
+        }
+        return _subjects.join(' and ') + " " + this.verbConjugate[this.verb](firstperson, plural);
+      } else {
+        return "---"
+      }
+    }
   }
 }
 </script>
